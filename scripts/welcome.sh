a0419536ba7cd90fbb02778e04cccae41d7827b9
Brad Jasper <bjasper@gmail.com>
2009-10-19 10:05:05 -0500

    first commit

---
+++ scripts/welcome.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Displayed when you login
+
+USERNAME=`whoami`
+
+echo
+echo "==========================================================="
+echo "  Welcome $USERNAME to $ENVSYNC_OS @$MACHTYPE"
+echo "==========================================================="
+echo
