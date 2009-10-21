cbefebcf796218be5895c43d1741745583929347
Brad Jasper <bjasper@gmail.com>
2009-10-20 23:59:55 -0500

    updated welcome script

--- init/welcome.sh
+++ init/welcome.sh
@@ -3,9 +3,20 @@
 # Displayed when you login
 
 USERNAME=`whoami`
+WELCOME_MSG="Unknown"
+
+case $ENVSYNC_OS in
+    mac)
+        WELCOME_MSG="Mac OSX"
+        ;;
+
+    linux)
+        WELCOME_MSG="Linux"
+        ;;
+esac
 
 echo
 echo "==========================================================="
-echo "  Welcome $USERNAME to $ENVSYNC_OS @$MACHTYPE"
+echo "  Welcome $USERNAME to $WELCOME_MSG @$MACHTYPE"
 echo "==========================================================="
 echo
