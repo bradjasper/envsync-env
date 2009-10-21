be73c351e07b70fc9bc73a6fb645c12a8a4c10e7
Brad Jasper <bjasper@gmail.com>
2009-10-21 00:21:39 -0500

    cleaned up git leftovers

--- scripts/utils.sh
+++
@@ -1,6 +0,0 @@
-# Various system wide utilities
-
-ffpy() { sudo find . -name \*.py 2> /dev/null | xargs grep "$1" | tr -d "\t"; }
-ffphp() { sudo find . -name \*.php 2> /dev/null | xargs grep "$1" | tr -d "\t"; }
-ff() { sudo find . 2> /dev/null | xargs grep "$1" | tr -d "\t"; }
-super_kill() { kill -9 `ps -ef|grep "$1"|grep -v grep|awk '{print $2}'`; }
