18a909209c05291292cf5b33cc9e69753cdc3298
Brad Jasper <bjasper@gmail.com>
2010-08-30 19:52:05 -0500

    updated environment

--- init/utils.sh
+++ init/utils.sh
@@ -3,4 +3,4 @@
 gfpy() { sudo find . -name \*.py 2> /dev/null | xargs grep "$1" | tr -d "\t"; }
 gfphp() { sudo find . -name \*.php 2> /dev/null | xargs grep "$1" | tr -d "\t"; }
 gf() { sudo find . 2> /dev/null | xargs grep "$1" | tr -d "\t"; }
-super_kill() { kill -9 `ps -ef|grep "$1"|grep -v grep|awk '{print $2}'`; }
+super_kill() { sudo kill -9 `ps -ef|grep "$1"|grep -v grep|awk '{print $2}'`; }
